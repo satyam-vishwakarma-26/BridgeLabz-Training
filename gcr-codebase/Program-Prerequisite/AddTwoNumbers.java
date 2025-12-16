6fd5d1ea2a491e6edfdca1d4a12f328342091d5f
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-16 16:50:25 +0530

    [Satyam] : Added AddTwoNumbers Program

---
+++ gcr-codebase/Program-Prerequisite/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AddTwoNumbers{
+public static void main(String[] args){
+Scanner sc=new Scanner (System.in);
+
+int a=sc.nextInt();
+int b=sc.nextInt();
+
+System.out.println("Addition of two number is : "+(a+b));
+
+}
+}
