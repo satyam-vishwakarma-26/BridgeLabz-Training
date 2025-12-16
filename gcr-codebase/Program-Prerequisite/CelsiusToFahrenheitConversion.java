d148a2a5378ff57586d993f2cb08cf15db5bf070
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-16 17:03:26 +0530

    [Satyam] : Added Celsius to Fahrenheit Conversion

---
+++ gcr-codebase/Program-Prerequisite/CelsiusToFahrenheitConversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CelsiusToFahrenheitConversion{
+ public static void main(String [] args){
+ Scanner sc=new Scanner (System.in);
+ System.out.print("Enter the Temperature in Celsius : ");
+ int celsius=sc.nextInt();
+ 
+ double fahrenheit= (celsius * 9/5) + 32;
+ 
+ System.out.println("Temperature in Fahrenheit is: "+fahrenheit);
+ }
+}
