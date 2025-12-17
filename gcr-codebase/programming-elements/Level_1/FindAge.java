f27eafa4b2ed3351f679f20173fd914373e8af53
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-17 15:14:17 +0530

    [Satyam] : Added, Find Age Of Harry

---
+++ gcr-codebase/programming-elements/Level_1/FindAge.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class FindAge{
+
+  public static void main(String [] args){
+  Scanner sc=new Scanner (System.in);
+  System.out.print("Enter Current Year: ");
+  int CurrentYear=sc.nextInt();
+  System.out.println("Harry Age is: "+(CurrentYear-2000));
+ }
+}
+  
