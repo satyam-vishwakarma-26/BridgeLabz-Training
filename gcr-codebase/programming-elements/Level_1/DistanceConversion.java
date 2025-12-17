4e98ebb576ab1462fe7777cacc2fe65a98550afc
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-17 15:44:48 +0530

    [Satyam]: Added, Distance conversion form Kilometer to Miles

---
+++ gcr-codebase/programming-elements/Level_1/DistanceConversion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class DistanceConversion{
+ public static void main(String [] args){
+ 
+// Distance in Kilometers
+ double Kilometers=10.8;
+
+// distance Calculation
+ double Miles=(Kilometers*1.6);
+
+//Printing the Distance in miles 
+ System.out.println("The distance " +Kilometers+ ", in miles is "+Miles);
+
+}
+}
