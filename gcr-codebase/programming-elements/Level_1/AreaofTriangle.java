2208026ed461b08e3fb5a331b3ad1b0b677471fb
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-17 20:55:25 +0530

    [Satyam]: Added, Area of Triangle

--- gcr-codebase/programming-elements/Level_1/AreaofTriangle.java
+++ gcr-codebase/programming-elements/Level_1/AreaofTriangle.java
@@ -16,10 +16,10 @@ public class AreaofTriangle{
         double areaInSqInches = 0.5 * base * height;
 
         // Convert square inches to square centimeters
-          double areaInSqCm = areaInSqInches * 6.4516;
+        double areaInSqCm = areaInSqInches * 6.4516;
 
-        // Output of area
-        System.out.println("Area of triangle in inches and centimeters:");
+        // Output
+        System.out.println("Area of triangle:");
         System.out.println("In square inches = " + areaInSqInches);
         System.out.println("In square centimeters = " + areaInSqCm);
   }
