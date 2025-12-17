eb447694b066d598bdec9051cc309f81304e56ac
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-17 17:07:41 +0530

    [Satyam]: Added, Student Fee discount By taking user inputs

---
+++ gcr-codebase/programming-elements/Level_1/StudentFee.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class StudentFee{
+ public static void main(String [] args){
+ 
+//Scanner class for user inputs
+ Scanner inputs=new Scanner (System.in);
+// Student Fee
+System.out.print("Enter Student fee: ");
+ double fee=inputs.nextDouble();
+//DiscountPercent 
+System.out.print("Enter Fee Discount Percent: ");
+ double DiscountPercent=inputs.nextDouble();
+
+
+// Discount Computation
+  double Discount=(fee*DiscountPercent)/100;
+//Remaining fee that needs to be paid
+ double Remaining_Fee=fee-Discount;
+
+//Printing Pen distribution 
+ System.out.println("The discount amount is INR: " +Discount+ ",  and final discounted fee is INR: "+Remaining_Fee);
+
+
+  }
+}
