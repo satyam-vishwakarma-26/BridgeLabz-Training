c17362fa5fcf9b5e8a75d185f0992d72ff99eeee
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-17 15:26:17 +0530

    [Satyam]: Added, Find PCM Average

---
+++ gcr-codebase/programming-elements/Level_1/FindPCMAverage.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FindPCMAverage{
+ public static void main(String [] args){
+ Scanner sc=new Scanner (System.in);
+ int Math_Marks=94;
+ int Physics_Marks=95;
+ int Chemistry_Marks=96;
+ 
+ double Avg_PCM=(Math_Marks+Physics_Marks+Chemistry_Marks)/3;
+ 
+  System.out.println("Average Of PCM: "+Avg_PCM);
+ }
+}
