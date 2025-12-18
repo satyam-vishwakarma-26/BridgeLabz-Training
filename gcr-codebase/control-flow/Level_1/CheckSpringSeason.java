89d692119fcbdb44221796d8db5588a2a6a70239
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-18 15:25:47 +0530

    [Satyam]: Added, Command line input

--- gcr-codebase/control-flow/Level_1/CheckSpringSeason.java
+++ gcr-codebase/control-flow/Level_1/CheckSpringSeason.java
@@ -3,11 +3,15 @@ public class CheckSpringSeason {
 
    public static void main (String [] args){
    
-   //Taking user inputs
-   Scanner inputs=new  Scanner(System.in);
-   System.out.print("Enter Month number, and Day: ");
-   int month=inputs.nextInt();
-   int day=inputs.nextInt();
+   //Taking user inputs by command line 
+        if (args.length < 2) {
+            System.out.println("Enter month and Day ");
+            return;
+        }
+
+        // Taking input from command line
+        int month = Integer.parseInt(args[0]);
+        int day = Integer.parseInt(args[1]);
    
    //checking SpringSeason 
    if((month == 3 && day >= 20) ||(month == 4) ||(month == 5) ||(month == 6 && day <= 20)){
