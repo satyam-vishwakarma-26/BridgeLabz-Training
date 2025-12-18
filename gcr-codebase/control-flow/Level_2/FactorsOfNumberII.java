44097c4a193edbb00054ee79a07a333c4c0d6243
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-18 23:02:29 +0530

    [Satyam]: Added, Factors Of Number using while loop

---
+++ gcr-codebase/control-flow/Level_2/FactorsOfNumberII.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class FactorsOfNumberII{
+
+    public static void main(String[]args){
+        
+        //Scanner class and object
+        Scanner sc=new Scanner(System.in);
+        
+        //Taking user input
+        System.out.print("Enter a number: ");
+        int number=sc.nextInt();
+        
+        //check for positive integer
+        if(number>0){
+            System.out.println("Factors of "+number+" are:");
+            int i=1;
+            while(i<=number){
+                if(number%i==0){
+                    System.out.println(i);
+                }
+                i++;
+            }
+        }else{
+            System.out.println("Please enter a positive integer");
+        }
+    }
+}
