eeff231924f0ac8965bf984049495c03a1d13ae5
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-18 12:36:53 +0530

    [Satyam]: Added, area of triangle

---
+++ gcr-codebase/programming-elements/Level_2/AreaofTriangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class AreaofTriangle{
+ public static void main(String [] args){
+ 
+      //Scanner class for user inputs
+      Scanner inputs=new Scanner (System.in);
+
+       // Height of Triangle
+       System.out.print("Enter Height in Inches: ");
+       double height=inputs.nextDouble();
+      //Base of Triangle
+      System.out.print("Enter Base in Inches: ");
+      double base=inputs.nextDouble();
+
+        // Area in square inches
+        double areaInSqInches = 0.5 * base * height;
+
+        // Convert square inches to square centimeters
+        double areaInSqCm = areaInSqInches * 6.4516;
+
+        // Output
+        System.out.println("Area of triangle:");
+        System.out.println("In square inches = " + areaInSqInches);
+        System.out.println("In square centimeters = " + areaInSqCm);
+  }
+}
