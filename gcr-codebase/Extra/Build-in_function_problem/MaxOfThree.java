a274c8389e1dcf985fa5a8e7cc84c14e7ed90296
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-24 12:42:55 +0530

    [

---
+++ gcr-codebase/Extra/Build-in_function_problem/MaxOfThree.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class MaxOfThree{
+    //take input
+    public static int takeInput(Scanner sc,String msg){
+        System.out.print(msg);
+        return sc.nextInt();
+    }
+    //find maximum
+    public static int findMax(int a,int b,int c){
+        int max=a;
+        if(b>max){
+            max=b;
+        }
+        if(c>max){
+            max=c;
+        }
+        return max;
+    }
+    public static void main(String[] args){
+	    //Creating Scanner class, object and taking user inputs 
+        Scanner sc=new Scanner(System.in);
+        int a=takeInput(sc,"Enter first number: ");
+        int b=takeInput(sc,"Enter second number: ");
+        int c=takeInput(sc,"Enter third number: ");
+        int max=findMax(a,b,c);
+        System.out.println("Maximum number is: "+max);
+    }
+}
