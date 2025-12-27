2708e775a37d00e03c86708e9e8fbf02a63b7779
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-27 22:03:18 +0530

    [Satyam] : Added, Book and BookDriver class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/levelone/BookDriver.java
@@ -0,0 +1,10 @@
+package com.constructor.levelone;
+
+public class BookDriver {
+    public static void main(String[] args) {
+    	
+    	//making the book object and calling the display method 
+        Book b = new Book("Clean Code", "Satyam1", 650);
+        b.display();
+    }
+}
