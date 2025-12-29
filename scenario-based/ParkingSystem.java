0939070c149b422b81a7252220b3a460eda3b760
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-29 16:57:45 +0530

    [Satyam] : Changes the formatting of the program

--- scenario-based/ParkingSystem.java
+++ scenario-based/ParkingSystem.java
@@ -4,7 +4,7 @@ public class ParkingSystem{
     static int currentOccupancy=0;
     static int totalSlots=5;
 
-    //park vehicle
+    //park vehicle method and checking the condition inside that 
     public static void park(){
         if(currentOccupancy<totalSlots){
             System.out.println("You can park your vehicle");
@@ -14,16 +14,18 @@ public class ParkingSystem{
         }
     }
 
-    //show occupancy
+    //showing occupancy of the park lot 
     public static void showOccupancy(){
         System.out.println("Available slots are: "+(totalSlots-currentOccupancy));
     }
 
     public static void main(String[] args){
 
+		//Creating the Scanner object
         Scanner sc=new Scanner(System.in);
         System.out.println("Total parking slots: "+totalSlots);
 
+		//Running the loop for operations 
         while(true){
 
             if(currentOccupancy==totalSlots){
@@ -31,6 +33,7 @@ public class ParkingSystem{
                 break;
             }
 
+			//Giving choices to the user
             System.out.println("\nEnter Your Choice:");
             System.out.println("1. Park");
             System.out.println("2. Exit");
@@ -38,6 +41,7 @@ public class ParkingSystem{
 
             int choice=sc.nextInt();
 
+			//Calling method according to the choices 
             switch(choice){
                 case 1:
                     park();
