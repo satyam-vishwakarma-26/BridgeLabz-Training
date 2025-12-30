2e78c96c4bb68d3fa99df203e46424ad329120c3
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-30 16:51:35 +0530

    [Satyam] : Added, Changes Code indentation

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Objectmodeling/levelone/School.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Objectmodeling/levelone/School.java
@@ -1,5 +1,18 @@
 package com.Objectmodeling.levelone;
 
-public class School {
+import java.util.ArrayList;
 
+
+class School{
+    String schoolName;
+    ArrayList<Student> students = new ArrayList<>();
+
+    
+    public School(String schoolName){
+        this.schoolName=schoolName;
+    }
+    //adding students 
+    public void addStudent(Student student){
+        students.add(student);
+    }
 }
--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Objectmodeling/levelone/Student.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Objectmodeling/levelone/Student.java
@@ -1,5 +1,25 @@
 package com.Objectmodeling.levelone;
 
-public class Student {
+import java.util.ArrayList;
 
+
+class Student{
+    String name;
+    ArrayList<Course> courses = new ArrayList<>();
+
+    public Student(String name){
+        this.name=name;
+    }
+    //Student enroll in course 
+    public void enrollCourse(Course course){
+        courses.add(course);
+        course.addStudent(this);
+    }
+    //showing course details 
+    public void showCourses(){
+        System.out.println("Student: "+name);
+        for(Course c:courses){
+            System.out.println("Enrolled in: "+c.courseName);
+        }
+    }
 }
