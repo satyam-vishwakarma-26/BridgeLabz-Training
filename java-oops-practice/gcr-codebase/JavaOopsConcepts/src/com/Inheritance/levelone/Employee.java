e4542ac9dcb1d3e45e459fcb9d142a0d03df630e
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2025-12-31 16:13:51 +0530

    [Satyam] : Added, Developer, Employee, Manager, EmployeeManagementSystem which shows the inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/levelone/Employee.java
@@ -0,0 +1,33 @@
+package com.Inheritance.levelone;
+
+//Base class
+class Employee{
+  String name;
+  int id;
+  double salary;
+
+  Employee(String name,int id,double salary){
+      this.name=name;
+      this.id=id;
+      this.salary=salary;
+  }
+
+  void displayDetails(){
+      System.out.println("Name: "+name);
+      System.out.println("ID: "+id);
+      System.out.println("Salary: "+salary);
+  }
+}
+
+//Manager subclass
+
+
+//Developer subclass
+
+
+//Intern subclass
+
+
+//Main class
+
+
