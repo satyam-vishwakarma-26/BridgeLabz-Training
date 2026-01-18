141fa4e30fe22bc13c4180b139a394260063d61b
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2026-01-18 22:53:38 +0530

    [Satyam] : Added, MaxValueKey

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/collections/map/MaxValueKey.java
@@ -0,0 +1,29 @@
+package com.collections.map;
+import java.util.*;
+
+public class MaxValueKey {
+
+    // finds key with highest value
+    public static String getMaxKey(Map<String, Integer> map) {
+        String maxKey = null;
+        int maxValue = Integer.MIN_VALUE;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxValue) {
+                maxValue = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+        return maxKey;
+    }
+
+    //main method 
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 10);
+        map.put("B", 20);
+        map.put("C", 15);
+
+        System.out.println(getMaxKey(map));
+    }
+}
