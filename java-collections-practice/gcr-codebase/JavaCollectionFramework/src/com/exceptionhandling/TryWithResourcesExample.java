556a68e9fb6c9656cdc76ee823c3a0d65ef125fc
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2026-01-24 08:58:25 +0530

    [Satyam] : Added, TryWithResourcesExample

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/exceptionhandling/TryWithResourcesExample.java
@@ -0,0 +1,14 @@
+package com.exceptionhandling;
+import java.io.*;
+
+public class TryWithResourcesExample {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("userinfo.txt"))) {
+            String line = br.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
