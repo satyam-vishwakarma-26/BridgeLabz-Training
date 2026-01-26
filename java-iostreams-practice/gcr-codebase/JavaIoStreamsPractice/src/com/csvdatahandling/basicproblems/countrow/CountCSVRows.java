a97cd635d25d1cda81a7066b2ba4dedd663ce255
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2026-01-26 23:47:49 +0530

    [Satyam] : Added, CountRow

---
+++ java-iostreams-practice/gcr-codebase/JavaIoStreamsPractice/src/com/csvdatahandling/basicproblems/countrow/CountCSVRows.java
@@ -0,0 +1,36 @@
+
+package com.csvdatahandling.basicproblems.countrow;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CountCSVRows {
+
+    public static void main(String[] args) {
+
+    	String fileName = "src/com/csvdatahandling/basic_problem/countRow/employees.csv";
+
+        String line;
+        int count = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+
+            // Skip header row
+            br.readLine();
+
+            // Count remaining rows
+            while ((line = br.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    count++;
+                }
+            }
+
+            System.out.println("Number of records (excluding header): " + count);
+
+        } catch (IOException e) {
+            System.out.println("Error reading CSV file");
+            e.printStackTrace();
+        }
+    }
+}
