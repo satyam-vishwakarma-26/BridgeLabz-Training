131d77a7674f790aa4c53ca80a2ba276b5444e09
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2026-01-26 15:51:58 +0530

    [Satyam] : Added, LinkExtractor

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/regax/extractionproblems/LinkExtractor.java
@@ -0,0 +1,20 @@
+package com.regax.extractionproblems;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LinkExtractor {
+
+    public static void main(String[] args) {
+
+        String text = "Visit https://www.google.com and http://tit.edu.in for more info.";
+
+        String regex = "https?://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
