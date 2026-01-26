72d87608705954f5db8796ba744a370fd125a0cd
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2026-01-26 15:51:32 +0530

    [Satyam] : Added, EmailExtractor

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/regax/extractionproblems/EmailExtractor.java
@@ -0,0 +1,21 @@
+package com.regax.extractionproblems;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailExtractor {
+
+    public static void main(String[] args) {
+
+        String text = "Contact us at satyamvishwakarma0103@gmail.com or satyam.0191cs221184@technocratsgroup.edu.in";
+
+        String regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        //Finding and Matching the Pattern 
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
